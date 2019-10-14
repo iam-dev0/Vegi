96da740472e9876ec28df136d5f88f9207b0e330
awais000 <awaismanzoor00@gmail.com>
2019-10-14 19:00:16 +0500

    .gitignore change some  files

---
+++ server/config/config.js
@@ -0,0 +1,6 @@
+export const db_username='awais';
+export const db_password="Dec241997"
+export const URI = `mongodb+srv://${db_username}:${db_password}@vegi-7kvyw.mongodb.net/Vegi?retryWrites=true&w=majority`;
+export const GOOGLE_CLOUD_PROJECT_ID = "bittrade-252909"; //
+export const DEFAULT_BUCKET_NAME = "bittrade-storage1";
+export const Coinbase_API_Key="0a317cf6-1181-4327-a0fe-96b51cd4e9ce";
